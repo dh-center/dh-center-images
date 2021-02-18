280aff2bb59b97f729ef0b71d1611cece2202e49
ilyamore88 <themorozik@gmail.com>
2021-02-18 17:36:47 +0300

    add simple handler

--- src/components/Handler.tsx
+++ src/components/Handler.tsx
@@ -1,11 +1,38 @@
 import {ReactElement} from "react";
-import styled from "styled-components";
-
-const HandlerWrapper = styled.div`
-`;
+import {Button, Form, Input} from "antd";
 
 function Handler(): ReactElement {
-  return (<HandlerWrapper>Handler</HandlerWrapper>);
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  }
+
+  return (
+    <Form
+      name="handler"
+      onFinish={onFinish}
+    >
+      <Form.Item
+        label="Image key"
+        name="key"
+        rules={[
+          {
+            required: true,
+            message: 'Please input image key!'
+          }
+        ]}
+      >
+        <Input/>
+      </Form.Item>
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+        >
+          Handle image
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
 
 export default Handler;
