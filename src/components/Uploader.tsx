b4981f0db069b3a61b80ccd1861595b4ec4c00e6
ilyamore88 <themorozik@gmail.com>
2021-02-18 20:16:43 +0300

    uploader uploads images

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,8 +1,10 @@
-import {ReactElement} from "react";
-import {Upload} from "antd";
+import {ReactElement, useState} from "react";
+import {Alert, Upload} from "antd";
 import {InboxOutlined} from '@ant-design/icons';
 
 function Uploader(): ReactElement {
+  const [imageKey, setImageKey] = useState<string | null>(null);
+
   return (
     <div>
       <Upload.Dragger
@@ -11,13 +13,10 @@ function Uploader(): ReactElement {
         action={process.env.REACT_APP_UPLOAD_ENDPOINT}
         onChange={info => {
           const {status} = info.file;
-          if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-          }
           if (status === 'done') {
-            console.log(`${info.file.name} file uploaded successfully.`);
+            setImageKey(info.file.response.image_key);
           } else if (status === 'error') {
-            console.log(`${info.file.name} file upload failed.`);
+            setImageKey(null);
           }
         }}
       >
@@ -26,6 +25,12 @@ function Uploader(): ReactElement {
         </p>
         <p className="ant-upload-text">Click or drag image to this area to upload</p>
       </Upload.Dragger>
+      {imageKey && <Alert
+        message="Image uploaded"
+        description={`Your image key is '${imageKey}'`}
+        type="success"
+        showIcon
+      />}
     </div>
   );
 }
